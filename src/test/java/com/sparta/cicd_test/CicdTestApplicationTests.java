62b77ceb0ca55f31957a2afbf71bdfe2805cda3d
sion <topaegr3525@kakao.com>
2024-08-24 15:10:50 +0900

    first commit

---
+++ src/test/java/com/sparta/cicd_test/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cicd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
