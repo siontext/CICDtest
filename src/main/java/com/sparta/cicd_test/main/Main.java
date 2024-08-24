1897db09d2b2834b663e4ad53558f29aac8d03e8
sion <topaegr3525@kakao.com>
2024-08-24 20:06:24 +0900

    CICD 잘 작동하는지 테스트

---
+++ src/main/java/com/sparta/cicd_test/main/Main.java
@@ -0,0 +1,9 @@
+package com.sparta.cicd_test.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("He was car");
+    }
+}
